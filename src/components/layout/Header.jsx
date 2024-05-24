0bc426135b8944f8d122d2d89cf4c5ef086a0d38
ariframaps <ariframa@student.ub.ac.id>
2024-05-24 08:54:54 +0700

    hero background

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,12 +2,12 @@ import { DarkmodeToggle } from "../others/DarkmodeToggle"
 
 export const Header = () => {
     return (
-        <header className="font-poppins text-base">
+        <header className="z-[999] font-poppins text-base fixed left-0 right-0 text-slate-100 font-light">
             <div className="m-auto h-20 flex justify-between items-center max-w-screen-3xl">
                 {/* status */}
                 <div className="flex items-center">
                     Status :
-                    <span className="flex items-center ms-2 border px-2.5 py-1 rounded-lg">
+                    <span className="flex items-center ms-2 px-2.5 py-1 rounded-lg bg-white text-black">
                         Available
                         <svg className="ms-2 w-2.5 h-2.5 bg-green-400 rounded-full"></svg>
                     </span>
--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -1,5 +1,8 @@
 export const Home = () => {
     return (
-        <div>Home</div>
+        <div className="min-h-screen bg-[url('../assets/background/hero_image-light.webp')]">
+            {/* background dark layer */}
+            <div className="absolute left-0 bottom-0 top-0 w-1/2 bg-gradient-to-r from-black/75 from-30%"></div>
+        </div>
     )
 }
