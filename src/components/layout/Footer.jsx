76b5e5157a61a04fa04477fa46b12a3189efc027
ariframaps <ariframa@student.ub.ac.id>
2024-05-25 19:22:52 +0700

    footer responsive

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,12 +3,12 @@ import github from '../../assets/logo/github.svg';
 
 export const Footer = () => {
     return (
-        <footer className="relative z-[2] h-80 bg-slate-50">
+        <footer className="px-8 xl:px-0 relative z-[2] h-60 lg:h-80 bg-slate-50">
             <div className="flex items-center max-w-xl m-auto h-full">
-                <div className='flex justify-between w-full'>
-                    <div>
+                <div className='flex flex-col lg:flex-row justify-between w-full'>
+                    <div className='w-fit pb-4 border border-0 border-b-2 mb-4'>
                         <p className='font-poppins font-medium'>Connect with me</p>
-                        <div className='flex mt-[30px] gap-[17px]'>
+                        <div className='flex mt-4 lg:mt-[30px] gap-3 lg:gap-[17px]'>
                             <a href="https://linkedin.com/in/ariframaps" className='duration-100 hover:-translate-y-[3px] hover:drop-shadow-[0_3px_0px_rgba(0,0,0,1)]'>
                                 <img className='w-[32px]' src={linkedin} alt="Linkedin logo" />
                             </a>
@@ -19,7 +19,7 @@ export const Footer = () => {
                     </div>
                     <div>
                         <p className='font-poppins font-medium'>Let's work together!</p>
-                        <div className='font-lato mt-[30px] text-[#7D7D7D]'>
+                        <div className='font-lato mt-4 lg:mt-[30px] text-[#7D7D7D]'>
                             <p>Whatsapp: 081335618823</p>
                             <p>Email: ariframaputra33@gmail.com</p>
                         </div>
