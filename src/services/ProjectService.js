32ca9af5340fc415bb92589f077d15931ef1d8e0
ariframaps <ariframa@student.ub.ac.id>
2024-05-25 17:03:29 +0700

    failed fetch project error handling

--- src/services/ProjectService.js
+++ src/services/ProjectService.js
@@ -1,20 +1,32 @@
 export const GetProjects = async () => {
-    const response = await fetch('https://api.github.com/users/ariframaps/repos');
-    const result = await response.json();
-    const filteredResults = result.filter(result => result.homepage !== null);
-
     const projectsList = [];
-    filteredResults.forEach(repo => {
-        const project = {
-            name: repo.name,
-            description: repo.description,
-            topics: repo.topics,
-            web_link: repo.homepage,
-            thumbnail_url: `https://raw.githubusercontent.com/ariframaps/${repo.name}/master/public/thumbnail.png`
-        }
 
-        projectsList.push(project);
-    });
+    try {
+        const response = await fetch('https://api.github.com/users/ariframaps/repos');
+        const result = await response.json();
+        const filteredResults = result.filter(result => result.homepage !== null);
 
-    return projectsList;
+        filteredResults.forEach(repo => {
+            const project = {
+                name: repo.name,
+                description: repo.description,
+                topics: repo.topics,
+                web_link: repo.homepage,
+                thumbnail_url: `https://raw.githubusercontent.com/ariframaps/${repo.name}/master/public/thumbnail.png`
+            }
+            projectsList.push(project);
+        });
+
+        return projectsList;
+    } catch (err) {
+        const errFetch = [{
+            name: err.message,
+            description: '-',
+            topics: [],
+            web_link: '/',
+            thumbnail_url: 'https://raw.githubusercontent.com/ariframaps/lead_capture/master/images/features1.png'
+        }]
+        return errFetch;
+    }
+
 }
